51101e30917bd5d62c5d1bb7f67fde858d048ea0
vnp23 <vnp23@imperial.ac.uk>
2024-03-06 15:25:25 +0000

    unified azure storage everywhere

--- storage/azureStorage.py
+++ storage/azureStorage.py
@@ -1,5 +1,14 @@
 from azure.storage.blob import BlobServiceClient
 from os import environ
+from datetime import datetime, timedelta
+import logging
+
+# Disable info logs for the azure-storage-blob library
+(logging.getLogger("azure.core.pipeline.policies.http_logging_policy")
+ .setLevel(logging.WARNING))
+(logging.getLogger("azure.core.pipeline.transport.http_requests")
+ .setLevel(logging.WARNING))
+
 
 # Initialize the BlobServiceClient using the connection string
 connection_string = environ.get("AZURE_STORAGE_CONNECTION_STRING", '')
@@ -23,8 +32,32 @@ def download(local_path, blob_name):
         download_file.write(container_client.get_blob_client(
             blob_name).download_blob().readall())
 
+
 def delete(blob_name):
     blob_client = container_client.get_blob_client(blob_name)
     if blob_client.exists():
         blob_client.delete_blob()
 
+
+def clear_blobs(session, hours=1):
+    blob_list = container_client.list_blobs(name_starts_with=session)
+    current_time = datetime.now()
+    cutoff_time = current_time - timedelta(hours=hours)
+    error = None
+    num_deleted = 0
+
+    for blob in blob_list:
+        try:
+            ts = blob.name.strip(session).split('.')[0].split('-')
+            params = [int(ts[i]) for i in range(len(ts))]
+            if cutoff_time.day >= params[0] and \
+                    cutoff_time.hour >= params[1] and \
+                    cutoff_time.minute >= params[2] and \
+                    cutoff_time.second >= params[3]:
+                num_deleted += 1
+                delete(blob.name)
+        except Exception as e:
+            error = e
+    if error:
+        return error
+    return 'Deleted {} blob(s)'.format(num_deleted)
